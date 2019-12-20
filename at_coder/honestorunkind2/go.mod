c5bd5f9c7b2d80b62ca5daf7cde7c52d1859eff0
RyoTozawa <ryo.for.development@gmail.com>
2019-12-20 12:47:16 +0900

    save problem 3

---
+++ at_coder/honestorunkind2/go.mod
@@ -0,0 +1,3 @@
+module github.com/tozastation/comp_prog/at_coder/honestorunkind2
+
+go 1.13
---
+++ at_coder/honestorunkind2/main.go
@@ -0,0 +1,22 @@
+package honestorunkind2
+
+// Algo
+// 1. Nを受取る
+// 2. [N][N]
+
+type Point struct {
+	X string
+	Y string
+}
+
+func main() {
+	// read standard input
+	//stdin := bufio.NewScanner(os.Stdin)
+	//perPerson := 1
+	//N := stdin.Text()
+	//NNum, _ := strconv.Atoi(N)
+	//input := make([][]string, NNum)
+	//for stdin.{
+	//	text := stdin.Text()
+	//}
+}
